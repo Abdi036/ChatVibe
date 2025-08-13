aec2bbaa070a24dd056da2aebecc132aa15b2ae8
Abdi036 <abdikumela036@gmail.com>
2025-08-13 00:57:56 -1200

    fixed some features in Navbar and sidebar

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -43,7 +43,10 @@ const Navbar = () => {
                   <span className="hidden sm:inline">Profile</span>
                 </Link>
 
-                <button className="flex gap-2 items-center" onClick={logout}>
+                <button
+                  className="flex gap-2 items-center cursor-pointer bg-red-400 p-1 rounded-md shadow-lg"
+                  onClick={logout}
+                >
                   <LogOut className="size-5" />
                   <span className="hidden sm:inline">Logout</span>
                 </button>
