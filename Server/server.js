0aed16cd122d4729fd032122b10101a0a5a81278
Abdi036 <abdikumela036@gmail.com>
2025-08-20 18:10:11 +0300

    Add .gitignore, update axios and auth store for environment-based URLs, enhance server for production, and create package.json with build and start scripts

--- Server/server.js
+++ Server/server.js
@@ -6,16 +6,32 @@ import { connectDB } from "./utils/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./utils/socket.js";
+import path from "path";
+
 dotenv.config();
 const PORT = process.env.PORT || 3001;
+const __dirname = path.resolve();
 
-app.use(express.json({ limit: "10mb" })); // or higher if needed
+app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",  
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
+
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../Client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Client/dist/index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}...`);
