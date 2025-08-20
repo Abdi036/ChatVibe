6702e2dd3071a1c723c25c5f90e9f373d0177352
Abdi036 <abdikumela036@gmail.com>
2025-08-20 17:40:14 +0300

    Refactor server.js for improved structure and clarity

--- Server/server.js
+++ Server/server.js
@@ -12,12 +12,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json({ limit: "10mb" })); // or higher if needed
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "https://chat-vibe-six.vercel.app",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
