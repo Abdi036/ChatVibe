4deb9f69f7a60ece5ebfd9c415d4c419b3a84e86
Abdi036 <abdikumela036@gmail.com>
2025-08-20 17:36:23 +0300

    Update axios baseURL and CORS origin for production deployment

--- Client/src/libs/axios.js
+++ Client/src/libs/axios.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   // baseURL: "http://localhost:3000/api/v1",
-  // baseURL: "https://chatvibe-ryl5.onrender.com/api/v1",
-  baseURL: "https://chatvibe-ryl5.onrender.com/api/v1",
+  baseURL: "https://chatvibe-9w5q.onrender.com/api/v1",
   withCredentials: true,
 });
