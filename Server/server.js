97b7aac9e326516908fdafc637f19c5ba5e68ce9
Abdi036 <abdikumela036@gmail.com>
2025-08-27 01:07:55 +0300

    Refactor axios and auth store for consistent base URL handling; simplify CORS configuration in server.js

--- Server/server.js
+++ Server/server.js
@@ -15,12 +15,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/messages", messageRoutes);
